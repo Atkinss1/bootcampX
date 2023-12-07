ae0df01edc91c482dc23a02757628be0214dcaea
Atkinss1 <jordanatkins@live.com>
2023-12-07 06:11:10 -0700

    refactor: paramaterized query string

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,13 +8,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
 SELECT students.id AS id, students.name AS student, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = students.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
